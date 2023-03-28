23dbb5ac48adece6b733ce5bf65cc77544475933
Buterbrod-iku <melkov750@gmail.com>
2023-03-28 20:05:02 +0700

    final_version

---
+++ menu/js/sale_js.js
@@ -0,0 +1,10 @@
+function one() {
+	document.getElementById('day_sale_1').style.background = "#d1d9dc";
+	document.getElementById('day_sale_2').style.background = "#9d9c9a";
+	document.getElementById('p_price').innerHTML="14000р.";	
+}
+function two() {
+	document.getElementById('day_sale_1').style.background = "#9d9c9a";
+	document.getElementById('day_sale_2').style.background = "#d1d9dc";
+	document.getElementById('p_price').innerHTML="20000р.";	
+}
